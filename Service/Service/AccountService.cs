1ef2edabe1bdb89d1874b4a6a3e8dbec8220bd5d
duongmake <duongmake3@gmail.com>
2024-08-30 12:38:25 +0700

    commit Login

---
+++ Service/Service/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using ClotheBusinessObject.BusinessObject;
+using Repo.Interface;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepo _accountRepo;
+        public AccountService(IAccountRepo accountRepo)
+        {
+            _accountRepo = accountRepo;
+        }
+
+        public Account CheckLogin(string email, string password) => _accountRepo.CheckLogin(email, password);
+
+    }
+}
---
+++ Service/Service/JWTTokenService.cs
@@ -0,0 +1,84 @@
+﻿using ClotheBusinessObject.BusinessObject;
+using ClotheBusinessObject.ViewModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class JWTTokenService : IJWTTokenService
+    {
+        private readonly IConfiguration _config;
+        public JWTTokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string CreateJWTToken(Account account)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, account.AccountID.ToString()),
+                new Claim(ClaimTypes.Email, account.Email ?? ""),
+                new Claim(ClaimTypes.Role, account.Role?.RoleName ?? ""),
+            };
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.UtcNow.AddMinutes(120), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public AuthVM ParseJwtToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_config["Jwt:Key"]);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            SecurityToken validatedToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out validatedToken);
+            if (validatedToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            var tokenId = principal.FindFirst("jti");
+            var accountId = principal.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+            var emailClaim = principal.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            var roleClaim = principal.FindFirst("http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
+
+            if (tokenId == null || accountId == null || emailClaim == null || roleClaim == null)
+            {
+                throw new SecurityTokenException("Invalid token claims");
+            }
+
+            var account = new AuthVM
+            {
+                AccountID = new Guid(accountId.Value),
+                Email = emailClaim.Value,
+                RoleName = roleClaim.Value
+            };
+
+            return account;
+        }
+    }
+}

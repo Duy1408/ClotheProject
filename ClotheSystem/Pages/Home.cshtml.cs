bf7d262cfb6c6aa905f5e0ed4066275ac0402a3f
Hoang Nam <nampham13072@gmail.com>
2024-09-06 00:59:29 +0700

    Home

---
+++ ClotheSystem/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClotheSystem.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
